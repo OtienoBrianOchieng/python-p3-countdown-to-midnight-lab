1778de1adc8ac4fed38e8a9698144cd308d5c3b7
root <root@DESKTOP-UEO62DE.localdomain>
2023-11-27 11:22:50 +0300

    Complete countdown lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,13 @@
 # your code goes here!
+number = 10
+def countdown(number):
+    while number > 0:
+        print (f"{number} SECOND(S)!")
+        number -= 1
+    print ("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(number): 
+    while number > 0:
+        print (f"{number} SECOND(S)!")
+        number -= 1
+    print ("HAPPY NEW YEAR!")
